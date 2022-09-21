b7886f9e1f4f16555abf14d576f0994b0c8f38a8
stephanielee9 <stephanielee@uchicago.edu>
2022-09-21 14:03:30 -0500

    changed logo and found area for changing background (but seems to be inheriting something from somewhere else

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ html_static_path = ['_static']
 html_theme_options = {
     "logo_only": True,
 }
-html_logo = "_static/logos/superstaq-logo-v2.png"
+html_logo = "_static/logos/superstaq-logo-v3.png"
 html_css_files = [
     'css/custom-test.css',
 ]
