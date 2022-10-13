c8c1ead01b8f7bba745d3640f821dea5f4c6e259
stephanielee9 <stephanielee@uchicago.edu>
2022-10-13 15:31:19 -0500

    updating submodules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,4 +52,3 @@ exclude_patterns: List[str] = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
