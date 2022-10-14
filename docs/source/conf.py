9f04dbc2e4c52cfe2e7f60f08b3c5beaf7c6039c
stephanielee9 <stephanielee@uchicago.edu>
2022-10-14 17:09:44 -0500

    adding new css files and updating colors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,8 @@ html_theme_options = {
 }
 html_logo = "_static/logos/superstaq-logo-v3.png"
 html_css_files = [
+    # 'css/dss-colors.css',
     'css/custom-test.css',
-    'css/custom-pygments.css',
+    # 'css/custom-pygments.css',
 ]
 html_favicon = "_static/logos/superstaq-favicon.png"
